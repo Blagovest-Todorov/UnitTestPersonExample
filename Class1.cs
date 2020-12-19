15a8e49e89b33f03f4e788089005e7f4e43b1ad1
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-19 15:15:57 +0200

    init

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PeoplesRepo
+{
+    public class Person
+    {
+        public Person(string firstname, string lastname, int age)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Age = age;
+        }
+
+        public string Firstname { get; set; }
+        public string Lastname{ get; set; }
+        public int Age{ get; set; }
+
+        
+    }
+}
