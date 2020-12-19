669358e5886d24729f07cf95ecce4baa74510ece
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-19 15:17:23 +0200

    the TestProject

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeoplesRepo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PeoplesRepo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e6115e6-8ab9-4248-aaf0-f6d5ef849126")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PeoplesRepo.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void Test_CreatePersonShouldSetProperties()
+        {
+            // arrange
+
+            // act
+            // assert
+
+            var firstname = "Test";
+            var lastname = "Test1";
+            var age = 21;
+
+            // act
+            var person = new Person(firstname, lastname , age);
+
+            // assert
+
+            Assert.AreEqual(firstname, person.Firstname);
+            Assert.AreEqual(lastname, person.Lastname);
+            Assert.AreEqual(age, person.Age);
+            
+
+        }
+    }
+}
